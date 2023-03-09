9c5ea7a26b6e7c7541233de3e80f2e2c74ff4945
rahulpradhan5 <prahulkumar591@gmail.com>
2023-03-09 18:02:38 +0530

    checkepin and give package doen

--- magnus-4-3-23/src/app/generation-downline/generation-downline.component.ts
+++ magnus-4-3-23/src/app/generation-downline/generation-downline.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-generation-downline',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./generation-downline.component.css']
 })
 export class GenerationDownlineComponent implements OnInit {
+generation:any[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    const uid = sessionStorage.getItem('firebaseUserId');
+    this.http.get('http://moneysagaconsultancy.com/api/api/generationtree?user_id=ab00003')
+    .subscribe((data:any) => {
+      data.data.forEach((datas:any) => {
+        this.generation.push({ ['created_at']: datas.created_at,['owner']: datas.owner,['points']: datas.points,['position']: datas.position,['referrer_id']: datas.referrer_id,['user_id']: datas.user_id,['level']: '1' });
+        datas.subdata.forEach((daatas:any)=>{
+          this.generation.push({ ['created_at']: daatas.created_at,['owner']: daatas.owner,['points']: daatas.points,['position']: daatas.position,['referrer_id']: daatas.referrer_id,['user_id']: daatas.user_id,['level']: '2' });
+        })
+      });
+    })
   }
 
 }
--- magnus-4-3-23/src/app/member-epin-login/member-epin-login.component.ts
+++ magnus-4-3-23/src/app/member-epin-login/member-epin-login.component.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/auth.service';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-member-epin-login',
   templateUrl: './member-epin-login.component.html',
   styleUrls: ['./member-epin-login.component.css']
 })
+
 export class MemberEpinLoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient,private auth: AuthService, fStore: AngularFirestore, public auths: AngularFireAuth, private storage: AngularFireStorage, private router: Router) { }
+  myId:any;
+  mpin:any;
+  revenue:any;
+  payments:any;
+  packag:any;
+  packages: any = '';
+  user: any = '';
 
   ngOnInit(): void {
+    const uid = sessionStorage.getItem('firebaseUserId');
+    this.myId = uid;
+    this.http.get('http://moneysagaconsultancy.com/api/api/Package?user_id='+uid)
+    .subscribe((data:any) => {
+     if(data.epin == ''){
+      this.mpin = 'not generated';
+     }else{
+      this.mpin = data.epin[0]['epin'];
+     }
+     if(data.revenue == ''){
+      this.revenue = 0;
+     }else{
+      this.revenue = data.revenue;
+     }
+     this.payments = data.payments;
+
+     this.packag = data.package ;
+    })
   }
+  givepackage(event: Event){
+    event.preventDefault();
+    if(this.user === ''){
+      alert('enter user id');
+      return
+    }
+    if(this.packages === ''){
+      alert('select any packages');
+      return
+    }
+    if(this.mpin == 'not generated'){
+      alert('Please Go check epin and genrate the epin');
+      return
+    }
+
+    if(this.packages[1] >= this.revenue){
+      alert('you have low balance');
+      return
+    }
+    this.http.get('http://moneysagaconsultancy.com/api/api/givePackage?user_id='+this.myId+'&guid='+this.user+'&package='+this.packag[0]+'&amount='+this.packag[1])
+    .subscribe((data:any) => {
+      console.log(data);
+    })
+   
+  }
+ 
 
 }
