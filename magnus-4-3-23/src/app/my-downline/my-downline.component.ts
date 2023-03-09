ce0880dbe7157754929198d7f6b4aeea3c2eff3c
rahulpradhan5 <prahulkumar591@gmail.com>
2023-03-09 12:26:59 +0530

    changes

--- magnus-4-3-23/src/app/my-downline/my-downline.component.ts
+++ magnus-4-3-23/src/app/my-downline/my-downline.component.ts
@@ -7,13 +7,36 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./my-downline.component.css']
 })
 export class MyDownlineComponent implements OnInit {
-
+  totalLeftcount:any;
+  totalrightcount:any;
+  totalDataconleft:any;
+  totalDataconright:any;
+  totalleftpv:any;
+  totalrightpv:any;
+  totalconfirmleftpv:any;
+  totalcomfirmrightpv:any;
+  ownPv:any;
+  row:any;
+  leftdata:any;
+  rightdata:any;
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get('http://moneysagaconsultancy.com/api/api/totaluserdata?user_id=ab00003')
     .subscribe((data:any) => {
-      console.log(data);
+      this.totalLeftcount = data.total_details.leftdata.length;
+      this.totalrightcount = data.total_details.rightdata.length;
+      this.totalDataconleft = data.total_details.comfirm_leftcount;
+      this.totalDataconright = data.total_details.comfirm_rightcount;
+      this.totalleftpv = data.total_left_pv;
+      this.totalrightpv = data.total_right_pv;
+      this.totalconfirmleftpv = data.total_confirm_left_pv;
+      this.totalcomfirmrightpv = data.total_confirm_right_pv;
+      this.ownPv = data.ownPv;
+      this.leftdata = data.total_details.leftdata;
+      this.rightdata = data.total_details.rightdata;
+ 
+      console.log(this.leftdata);
     })
   }
 
